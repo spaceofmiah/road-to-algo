3647f35824261ad74551230b9c9e88f2a1d8de34
spaceofmiah <agbonzeosazuwa@gmail.com>
2022-10-16 15:09:07 +0100

    Algo: disjoint and unique

--- chap3_walkthrough.py
+++ chap3_walkthrough.py
@@ -53,4 +53,63 @@ def prefix_average3(S):
         A[j] = total / (j + 1)
     return A
 
-print(prefix_average3([10,2,3,5,60]))
+# print(prefix_average3([10,2,3,5,60]))
+
+
+# ---
+# Three-Way Set Disjointness
+# ---
+
+
+def disjoint1(A, B, C):
+    """Return True if there is no element common to all three lists."""
+    for a in A:
+        for b in B:
+            for c in C:
+                if a == b == c:
+                    return False
+    return True
+
+
+
+# ---
+# Asymptotic Analysis
+# ---
+
+def disjoint2(A, B, C):
+    """Return True if there is no element common to all three lists."""
+    for a in A:
+        for b in B:
+            if a == b:
+                for c in C:
+                    if a == c:
+                        return False
+    return True
+
+
+# ---
+# Element Uniqueness
+# ---
+
+def unique1(S):
+    """Return True if there are no duplicate element in sequence S"""
+    for j in range(len(S)):
+        for k in range(j + 1, len(S)):
+            if S[j] == S[k]:
+                return False
+    return True
+
+
+# ---
+# Sorting as a Solver
+# ---
+
+def unique2(S):
+    """Return True if there are no duplicate element in sequence S"""
+    temp = sorted(S)
+    for j in range(1, len(temp)):
+        if S[j - 1] == S[j]:
+            return False
+    return True
+
+
