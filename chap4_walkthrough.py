edc42db0202d7bfe91e3de1b5b495b15f41cfafc
spaceofmiah <agbonzeosazuwa@gmail.com>
2022-10-21 19:16:22 +0100

    Add: function which produce English Ruler

---
+++ chap4_walkthrough.py
@@ -0,0 +1,45 @@
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
+# print(factorial(10))
+
+# ---
+# English Ruler
+# ---
+
+def draw_line(tick_length, tick_label=''):
+    """Draw one line with given thick length 
+    (followed by optional label)
+    """
+    line = '-' * tick_length
+    if tick_label:
+        line += ' ' + tick_label
+    print(line)
+
+
+def draw_interval(center_length):
+    """Draw tick interval based on central tick 
+    length
+    """
+    if center_length > 0:
+        draw_interval(center_length - 1)
+        draw_line(center_length)
+        draw_interval(center_length - 1)
+    
+def draw_ruler(num_inches, major_length):
+    """Draw English ruler with given number of 
+    inches, major thick length
+    """
+    draw_line(major_length, '0')
+    for j in range(1, 1 + num_inches):
+        draw_interval(major_length - 1)
+        draw_line(major_length, str(j))
+
+    
+print(draw_ruler(3, 2))
+
