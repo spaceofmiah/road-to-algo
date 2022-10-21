c889d99eb16853c9bd7b39e646ee1833a749ae40
spaceofmiah <agbonzeosazuwa@gmail.com>
2022-10-21 19:43:18 +0100

    Add: function illustrating binary search

--- chap4_walkthrough.py
+++ chap4_walkthrough.py
@@ -41,5 +41,28 @@ def draw_ruler(num_inches, major_length):
         draw_line(major_length, str(j))
 
     
-print(draw_ruler(3, 2))
+# print(draw_ruler(3, 2))
+
+
+def binary_search(data, target, low, high):
+    """Return True if target is found in indicated portion of
+    a python list.
+    
+    The search only considers the portion from data[low] to 
+    data[high] inclusive.
+    """
+    if low > high:
+        return False
+    else:
+        mid = (low + high) // 2
+        if target == data[mid]:
+            return True
+        elif target < data[mid]:
+            return binary_search(data, target, low, mid -1)
+        else:
+            return binary_search(data, target, mid + 1, high)
+
+
+search_list=[2, 3, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 22, 23, 24, 25, 26, 28, 30, 31, 32, 34, 36]
+print(binary_search(search_list, 36, 0, len(search_list)-1))
 
